f4906f4aed7984f4eefe768e86f90527a23552bc
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-01 16:26:07 +0100

    Build a basic register form

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,7 +1,31 @@
+import { Button, Paper, TextField } from '@material-ui/core';
 import React from 'react';
+import Layout from './components/shared/Layout';
 
 const Register = () => {
-  return <p>Hello Register.tsx!</p>;
+  return (
+    <Layout>
+      <Paper>
+        <form method="post" onSubmit={e => e.preventDefault()}>
+          <TextField label="Username" name="username" />
+          <br />
+          <TextField label="Email" name="email" type="email" />
+          <br />
+          <TextField label="Password" name="password" type="password" />
+          <br />
+          <TextField
+            label="Confirm password"
+            name="passwordConfirmation"
+            type="password"
+          />
+          <br />
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Paper>
+    </Layout>
+  );
 };
 
 export default Register;
