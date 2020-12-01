17645aea325eff628c7aa27ab5d2f6d1fa4974e3
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-01 16:58:55 +0100

    Remove pass confirmation, add rhf

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,22 +1,44 @@
 import { Button, Paper, TextField } from '@material-ui/core';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Layout from './components/shared/Layout';
 
+interface RegisterFormFields {
+  username: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+const onSubmit = (data: RegisterFormFields) => {
+  console.log(data);
+};
+
 const Register = () => {
+  const { register, handleSubmit, errors } = useForm<RegisterFormFields>();
+
   return (
     <Layout>
       <Paper>
-        <form method="post" onSubmit={e => e.preventDefault()}>
-          <TextField label="Username" name="username" />
-          <br />
-          <TextField label="Email" name="email" type="email" />
-          <br />
-          <TextField label="Password" name="password" type="password" />
+        <form method="post" onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            label="Username"
+            name="username"
+            inputRef={register({ required: true, minLength: 3, maxLength: 20 })}
+          />
           <br />
           <TextField
-            label="Confirm password"
-            name="passwordConfirmation"
+            label="Email"
+            name="email"
+            type="email"
+            inputRef={register({ required: true })}
+          />
+          <br />
+          <TextField
+            label="Password"
+            name="password"
             type="password"
+            inputRef={register({ required: true, minLength: 6, maxLength: 64 })}
           />
           <br />
           <Button variant="contained" color="primary" type="submit">
