2097be256b79f4169b6dc1c7f3cb0870bcfb777a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-01 15:50:09 +0100

    Create home page

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,7 +1,35 @@
+import { Button, makeStyles, Paper, Typography } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Layout from './components/shared/Layout';
+
+const useStyles = makeStyles({
+  centeredText: {
+    textAlign: 'center'
+  }
+});
 
 const HomePage = () => {
-  return <p>Hello HomePage.tsx!</p>;
+  const styles = useStyles();
+
+  return (
+    <Layout>
+      <div className={styles.centeredText}>
+        <Typography variant="h1">Ask and be asked</Typography>
+        <Paper>
+          <Typography variant="h4">K&amp;A - Kuestion and Answer</Typography>
+          <Typography>A Q&amp;A site for you and your friends!</Typography>
+          <Link to="/register">
+            <Button>Register</Button>
+          </Link>
+          <Link to="/login">
+            <Button>Login</Button>
+          </Link>
+        </Paper>
+      </div>
+    </Layout>
+  );
 };
 
 export default HomePage;
