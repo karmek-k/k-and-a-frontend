5bb29719341e9a88142d2a669e3da914912130ba
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-01 20:59:17 +0100

    Connect the register form to the backend

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
-import RegisterForm from './components/Register/RegisterForm';
+import {
+  Dialog,
+  DialogTitle,
+  LinearProgress,
+  Typography
+} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import RegisterForm, {
+  RegisterFormFields
+} from './components/Register/RegisterForm';
 import Layout from './components/shared/Layout';
 
+interface RegisterResponse {
+  id: string;
+  username: string;
+}
+
 const Register = () => {
+  const [formFields, setFormFields] = useState<RegisterFormFields | null>(null);
+  const [response, setResponse] = useState<RegisterResponse | null>(null);
+  const [errorDialog, setErrorDialog] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!formFields) {
+      return;
+    }
+
+    axios
+      .post<RegisterResponse>(
+        'http://localhost:8000/api/users/create',
+        formFields
+      )
+      .then(res => setResponse(res.data))
+      .catch(() => {
+        setErrorDialog(true);
+        setFormFields(null);
+      });
+  }, [formFields]);
+
   return (
     <Layout>
-      <RegisterForm />
+      <RegisterForm setFormFields={setFormFields} />
+
+      {formFields && !response && <LinearProgress />}
+
+      <Dialog
+        onClose={() => setErrorDialog(false)}
+        aria-labelledby="error dialog"
+        open={errorDialog}
+      >
+        <DialogTitle>Error</DialogTitle>
+        <Typography>Something went wrong.</Typography>
+      </Dialog>
     </Layout>
   );
 };
