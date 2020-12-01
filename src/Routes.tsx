077c9d69cf19ee6d34891d4a9ebf712cfec17838
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-01 15:47:36 +0100

    Create Login page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from './routes/HomePage';
+import Login from './routes/Login';
 import NotFound from './routes/NotFound';
 import Register from './routes/Register';
 
@@ -12,6 +13,7 @@ const Routes = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
         <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
