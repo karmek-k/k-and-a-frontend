a45407629a715d84283a0692aff8dd85d287b301
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-02 20:05:05 +0100

    Connect the login form to the backend

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,11 +1,54 @@
-import { LinearProgress } from '@material-ui/core';
-import React, { useState } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  LinearProgress,
+  Typography
+} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import LoginForm, { LoginFormFields } from './components/Login/LoginForm';
 import Layout from './components/shared/Layout';
+import { Redirect } from 'react-router-dom';
+
+interface LoginResponse {
+  token: string;
+}
 
 const Login = () => {
   const [formFields, setFormFields] = useState<LoginFormFields | null>(null);
   const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
+  const [loggedIn, setLoggedIn] = useState<boolean>();
+  const [errorMsg, setErrorMsg] = useState<string>('');
+  const [errorStatus, setErrorStatus] = useState<number>(0);
+  const [errorDialog, setErrorDialog] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!formFields) {
+      return;
+    }
+
+    setButtonDisabled(true);
+
+    axios
+      .post<LoginResponse>('/api/users/login', formFields)
+      .then(res => {
+        axios.defaults.headers.common = {
+          Authorization: `Bearer ${res.data.token}`
+        };
+
+        setLoggedIn(true);
+      })
+      .catch(e => {
+        setErrorMsg(e.response.data.msg);
+        setErrorStatus(e.response.status);
+        setButtonDisabled(false);
+        setErrorDialog(true);
+      });
+  }, [formFields]);
+
+  if (loggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <Layout>
@@ -13,7 +56,15 @@ const Login = () => {
         setFormFields={setFormFields}
         buttonDisabled={buttonDisabled}
       />
-      <LinearProgress color="secondary" />
+      {buttonDisabled && <LinearProgress color="secondary" />}
+      <Dialog
+        onClose={() => setErrorDialog(false)}
+        aria-labelledby="error dialog"
+        open={errorDialog}
+      >
+        <DialogTitle>Error {errorStatus}</DialogTitle>
+        <Typography>{errorMsg}</Typography>
+      </Dialog>
     </Layout>
   );
 };
