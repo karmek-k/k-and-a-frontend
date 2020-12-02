394d76b39b929a4bce88d17a4662071179b462c3
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-02 19:22:35 +0100

    Create the login form

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { LinearProgress } from '@material-ui/core';
+import React, { useState } from 'react';
+import LoginForm, { LoginFormFields } from './components/Login/LoginForm';
+import Layout from './components/shared/Layout';
 
 const Login = () => {
-  return <p>Login page</p>;
+  const [formFields, setFormFields] = useState<LoginFormFields | null>(null);
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
+
+  return (
+    <Layout>
+      <LoginForm
+        setFormFields={setFormFields}
+        buttonDisabled={buttonDisabled}
+      />
+      <LinearProgress color="secondary" />
+    </Layout>
+  );
 };
 
 export default Login;
