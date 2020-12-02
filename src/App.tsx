cef23f14637bb663b427564fdcabb651f5fa7b1a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-02 18:51:25 +0100

    Fetch a CSRF token while loading the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Routes from './Routes';
 import './globalStyle.css';
+import axios from 'axios';
+
+interface CsrfTokenResponse {
+  csrfToken: string;
+}
 
 const App = () => {
+  useEffect(() => {
+    axios
+      .get<CsrfTokenResponse>('/csrf-token')
+      .then(
+        res => (axios.defaults.headers.post['CSRF-Token'] = res.data.csrfToken)
+      )
+      .catch(e => console.error(e.response));
+  });
+
   return (
     <>
       <Routes />
