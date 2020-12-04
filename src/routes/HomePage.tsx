cb775a69b848c2313bd1c69fe30070703b513e6d
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-04 14:01:52 +0100

    Style the home page

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,17 +1,37 @@
-import { Button, Paper, Typography } from '@material-ui/core';
+import { Button, makeStyles, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Layout from './components/shared/Layout';
 import useSharedStyles from './components/shared/styles';
 
+const useStyles = makeStyles({
+  header: {
+    margin: '.5em 0'
+  },
+  paperContainer: {
+    padding: '2em',
+    width: '60%',
+    margin: 'auto'
+  },
+  paperHeader: {
+    marginBottom: '.5em'
+  }
+});
+
 const HomePage = () => {
   const sharedStyles = useSharedStyles();
+  const styles = useStyles();
+
   return (
     <Layout className={sharedStyles.centeredText}>
-      <Typography variant="h1">Ask and be asked</Typography>
-      <Paper>
-        <Typography variant="h4">K&amp;A - Kuestion and Answer</Typography>
+      <Typography className={styles.header} variant="h1">
+        Ask and be asked
+      </Typography>
+      <Paper className={styles.paperContainer}>
+        <Typography className={styles.paperHeader} variant="h4">
+          K&amp;A - Kuestion and Answer
+        </Typography>
         <Typography>A Q&amp;A site for you and your friends!</Typography>
         <Button
           color="primary"
