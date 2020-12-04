1d1c3fba9cd907ac2b12bb68a47cabf668ccd5c5
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-04 14:14:11 +0100

    Move Paper to the parent component, change btn color

--- src/routes/components/Register/RegisterForm.tsx
+++ src/routes/components/Register/RegisterForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { Button, Paper, TextField } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 
 export interface RegisterFormFields {
   username: string;
@@ -23,44 +23,42 @@ const RegisterForm = (props: Props) => {
   };
 
   return (
-    <Paper>
-      <form method="post" onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          label="Username"
-          name="username"
-          inputRef={register({ required: true, minLength: 3, maxLength: 20 })}
-          error={Boolean(errors.username)}
-          helperText={errors.username && 'Must be between 3-20 characters'}
-        />
-        <br />
-        <TextField
-          label="Email"
-          name="email"
-          type="email"
-          inputRef={register({ required: true })}
-          error={Boolean(errors.email)}
-          helperText={errors.email && 'Email is required'}
-        />
-        <br />
-        <TextField
-          label="Password"
-          name="password"
-          type="password"
-          inputRef={register({ required: true, minLength: 6, maxLength: 64 })}
-          error={Boolean(errors.password)}
-          helperText={errors.password && 'Must be between 6-64 characters'}
-        />
-        <br />
-        <Button
-          variant="contained"
-          color="primary"
-          type="submit"
-          disabled={props.buttonDisabled}
-        >
-          Submit
-        </Button>
-      </form>
-    </Paper>
+    <form method="post" onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        label="Username"
+        name="username"
+        inputRef={register({ required: true, minLength: 3, maxLength: 20 })}
+        error={Boolean(errors.username)}
+        helperText={errors.username && 'Must be between 3-20 characters'}
+      />
+      <br />
+      <TextField
+        label="Email"
+        name="email"
+        type="email"
+        inputRef={register({ required: true })}
+        error={Boolean(errors.email)}
+        helperText={errors.email && 'Email is required'}
+      />
+      <br />
+      <TextField
+        label="Password"
+        name="password"
+        type="password"
+        inputRef={register({ required: true, minLength: 6, maxLength: 64 })}
+        error={Boolean(errors.password)}
+        helperText={errors.password && 'Must be between 6-64 characters'}
+      />
+      <br />
+      <Button
+        variant="contained"
+        color="secondary"
+        type="submit"
+        disabled={props.buttonDisabled}
+      >
+        Submit
+      </Button>
+    </form>
   );
 };
 
