25d477f562f61d6bcd2a5080fe23ff044e6ca296
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-05 18:16:42 +0100

    Fetch questions from the backend

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,18 +1,48 @@
 import { Grid, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import QuestionFeed from './components/Dashboard/QuestionFeed';
 import Layout from './components/shared/Layout';
 
+export interface Question {
+  _id: string;
+  tags?: string[];
+  title: string;
+  description?: string;
+  datePosted: Date;
+  posterId: string;
+  recipientId: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    axios.get<Question[]>('/api/questions/latest').then(res => {
+      setQuestions(res.data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return (
+      <Layout>
+        <Typography variant="h2">Loading...</Typography>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
       <Typography variant="h2">Welcome</Typography>
       <Grid container spacing={4}>
         <Grid item md={4}>
-          <QuestionFeed />
+          <QuestionFeed questions={questions} />
         </Grid>
         <Grid item md={8}>
-          <QuestionFeed />
+          <QuestionFeed questions={questions} />
         </Grid>
       </Grid>
     </Layout>
--- src/routes/components/Dashboard/QuestionFeed.tsx
+++ src/routes/components/Dashboard/QuestionFeed.tsx
@@ -1,29 +1,19 @@
 import { Paper, Typography } from '@material-ui/core';
 import React from 'react';
 
-const QuestionFeed: React.FC = () => {
+import { Question } from '../../Dashboard';
+
+interface Props {
+  questions: Question[];
+}
+
+const QuestionFeed: React.FC<Props> = (props: Props) => {
   return (
     <Paper>
       <Typography variant="h4">Recent questions</Typography>
-      <Typography>
-        Similique libero consequuntur aut ducimus unde tenetur. Aut deleniti
-        nihil rerum quos neque. Assumenda omnis qui aut aut harum architecto
-        officiis eos. Explicabo voluptatem sequi dolorem quia provident autem.
-        Ab et temporibus veritatis tenetur consequatur mollitia doloremque
-        cumque. Ut consequatur consequuntur minus laudantium similique tempora.
-        Deleniti ea ea commodi fugit fugit pariatur unde quis. Aut pariatur
-        recusandae rerum facere molestiae excepturi. Assumenda harum dolor rerum
-        voluptatibus necessitatibus et expedita quo. Quod distinctio quia
-        dolorem ex commodi quibusdam quidem eos. Aliquam illum est asperiores.
-        Enim aspernatur veritatis numquam explicabo voluptates. Nisi eligendi
-        qui quia porro facere. Sit est quidem rem iusto dolorem quia soluta
-        quasi. Temporibus saepe perferendis et unde maiores nesciunt nisi hic.
-        Quis aut dolorem voluptatem cum doloribus inventore. Est aut velit non.
-        Aut ut libero tenetur repellat tenetur deleniti nemo pariatur. Doloribus
-        dolorem omnis consequatur corporis. Itaque possimus voluptates aliquam
-        sequi porro inventore explicabo. Aut itaque est libero. Mollitia debitis
-        reprehenderit necessitatibus est temporibus.
-      </Typography>
+      {props.questions.map(q => (
+        <Typography key={q._id}>{q.title}</Typography>
+      ))}
     </Paper>
   );
 };
